11a798b242283c2d96c7b84ef1a380a0a7f46fe2
AshiqG <Ashiqarib@gmail.com>
2024-05-31 20:19:34 -0400

    UPDATED PROJECTS SECTION

--- resume/src/containers/skillset/Skillset.jsx
+++ resume/src/containers/skillset/Skillset.jsx
@@ -1,8 +1,66 @@
 import React from 'react'
+import styles from '../skillset/skillset.csss'
 
 const Skillset = () => {
   return (
-    <div>Skillset</div>
+    <div class="skillset__container">
+
+      <h2>SKILLSET</h2>
+
+      <h3>PROGRAMMING</h3>
+      <ul class="skillset__container-prog">
+          <li>JAVA</li>
+          <li>C++</li>
+          <li>C</li>
+          <li>PYTHON</li>
+          <li>GIT</li>
+      </ul>
+
+      <h3>FRONT END DEVELOPMENT</h3>
+      <ul class="skillset__container-WebDev">
+          <li>HTML5</li>
+          <li>CSS3</li>
+          <li>JAVASCRIPT</li>
+          <li>TYPESCRIPT</li>
+          <li>NODE.JS</li>
+          <li>REACTJS</li>
+          <li>BOOTSTRAP</li>
+          <li>TAILWIND</li>
+          <li>MATERIALIZE</li>
+          <li>FIGMA</li>
+      </ul>
+
+      <h3>ARTIFICIAL INTELLIGENCE</h3>
+      <ul class="skillset__container-ai">
+          <li>TENSORFLOW</li>
+          <li>SCI-KIT LEARN</li>
+          <li>WEIGHTS & BIASES</li>
+          <li>PANDAS</li>
+          <li>OPENCV</li>
+      </ul>
+
+      <h3>ROBOTICS & CIRCUITRY</h3>
+      <ul class="skillset__container-robotics">
+          <li>UBUNTU LINUX</li>
+          <li>ROS</li>
+          <li>ARDUINO</li>
+          <li>RASPBERRY PI</li>
+          <li>SOLDERING</li>
+          <li>3D PRINTING</li>
+          <li>ULTIMAKER CURA</li>
+          <li>TINKER CAD</li>
+          <li>AUTOCAD</li>
+          <li>DOCKER</li>
+      </ul>
+
+      <h3>LINGUISTICS</h3>
+      <ul class="skillset__container-robotics">
+          <li>ENGLISH</li>
+          <li>FRENCH</li>
+          <li>BENGALI</li>
+      </ul>
+
+    </div>
   )
 }
 
