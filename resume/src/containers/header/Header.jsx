e58da30ea17fa6175584040b38f0e9e4743a343e
AshiqG <Ashiqarib@gmail.com>
2024-06-16 11:14:15 -0400

    UPDATED HEADER
    FUTURE NOTES:
    - SEPARATE HEADER FROM NAVBAR
    - MAKE A GREETING SECTION / HERO
    - LOCK NAVBAR TO TOP OF SCREEN

--- resume/src/containers/header/Header.jsx
+++ resume/src/containers/header/Header.jsx
@@ -4,29 +4,16 @@ const Header = () => {
   return (
     <div>
 
-      <div className="navbar__container">
-        <ul className="navbar">
-          <li><div className="navbar-home"><a href="#header">LANDING PAGE</a></div></li>
-          <li><div className="navbar-skills"><a href="#skills">SKILLS</a></div></li>
-          <li><div className="navbar-projects"><a href="#projects">PROJECTS</a></div></li>
-        </ul>
-      </div>
-
-      <div className="home">
-        <div className="home__greeting">
-          <h1>HEY, I'M ASHIQ!</h1>
-          <h2>I'M A 2ND YEAR CANADIAN SOFTWARE ENGINEERING STUDENT</h2>
-        </div>
-
-        <div className="home__contact">
-          <h2>CONTACT ME</h2>
-          <button>GMAIL</button>
-          <button>LINKEDIN</button>
-          <button>GITHUB</button>
-          <button>LEETCODE</button>
-        </div>
-      </div>
-
+      <div class="Navbar">
+        <div><span><img src="/assets/landing-page_logo.png" alt="HOME"></span>HOME</div>
+        <div><span><img src="/assets/skills_logo.png" alt="SKILLS"></span>SKILLS</div>
+        <div><span><img src="/assets/projects_icon.png" alt="PROJECTS"></span>PROJECTS</div>
+        <div><span><img src="/assets/gmail_logo.png" alt="EMAIL"></span>EMAIL</div>
+        <div><span><img src="/assets/linkedIn_logo.png" alt="LINKEDIN"></span>LINKEDIN</div>
+        <div><span><img src="/assets/github_logo.png" alt="GITHUB"></span>GITHUB</div>
+        <div><span><img src="/assets/leetcode_logo.png" alt="LEETCODE"></span>LEETCODE</div>
+      </div>  
+      
     </div>
   )
 }
